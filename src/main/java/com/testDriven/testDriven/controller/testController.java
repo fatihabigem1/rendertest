c7f72383c8700ed9dcf2b9187857cd506de45b2e
listenmusic410 <listenmusic410@gmail.com>
2024-01-13 18:48:51 +0100

    DAL added

--- src/main/java/com/testDriven/testDriven/controller/testController.java
+++ src/main/java/com/testDriven/testDriven/controller/testController.java
@@ -1,11 +1,16 @@
 package com.testDriven.testDriven.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.testDriven.testDriven.dal.CustomObject;
 
 @RestController
 @RequestMapping("/api")
@@ -37,4 +42,47 @@ public class testController {
         }
     }
 	
+	   @GetMapping("/example")
+	    public ResponseEntity<String> getExample() {
+	        String responseBody = "This is an example response";
+	        return ResponseEntity.ok(responseBody);
+	    }
+	   
+	   
+	   @GetMapping("/custom")
+	    public ResponseEntity<List<CustomObject>> getCustomObjects() {
+	        // Assume CustomObject is a custom class
+	        List<CustomObject> customObjectList = new ArrayList<>();
+	        // Add some custom objects to the list (can be fetched from a database or any other source)
+	        customObjectList.add(new CustomObject("1", "First object"));
+	        customObjectList.add(new CustomObject("2", "Second object"));
+	        // Return the list in the ResponseEntity with a 200 OK status
+	        return ResponseEntity.ok(customObjectList);
+	    }
+	
 }
+
+
+
+
+
+/*
+
+…or create a new repository on the command line
+
+echo "# rendertest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/fatihabigem1/rendertest.git
+git push -u origin main
+
+
+
+…or push an existing repository from the command line
+git remote add origin https://github.com/fatihabigem1/rendertest.git
+git branch -M main
+git push -u origin mai
+
+*/
